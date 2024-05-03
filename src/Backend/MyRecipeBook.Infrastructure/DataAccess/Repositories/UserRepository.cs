efcdf2d72bc3059c16fdc940c5da19e52268462c
Patrik Oliveira <patrikro.oliveira@gmail.com>
2024-05-02 23:48:10 -0300

    feat: add user repository and dbcontext

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Repositories.User;
+
+namespace MyRecipeBook.Infrastructure.DataAccess.Repositories;
+
+public class UserRepository : IUserWriteOnlyRepository, IUserReadOnlyRepository
+{
+    private readonly MyRecipeBookDbContext dbContext;
+
+    public UserRepository(MyRecipeBookDbContext dbContext) => this.dbContext = dbContext;
+
+    public async Task Add(User user) => await dbContext.AddAsync(user);
+
+    public async Task<bool> ExistActiveUserWithEmail(string email) => await dbContext.Users.AnyAsync(user => user.Email.Equals(email) && user.Active);
+}
