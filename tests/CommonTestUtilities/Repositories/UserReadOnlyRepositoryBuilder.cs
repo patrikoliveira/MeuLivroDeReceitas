38ddfe05362c9341eb26d695eba825edc83d73df
Patrik Oliveira <patrikro.oliveira@gmail.com>
2024-05-05 18:37:09 -0300

    test: adding tests for the use case of registering user with mock repositories

---
+++ tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using MyRecipeBook.Domain.Repositories.User;
+
+namespace CommonTestUtilities.Repositories;
+
+public class UserReadOnlyRepositoryBuilder
+{
+    private readonly Mock<IUserReadOnlyRepository> repository;
+
+    public UserReadOnlyRepositoryBuilder() => repository = new Mock<IUserReadOnlyRepository>();
+
+    public void ExistActiveUserWithEmail(string email)
+    {
+        repository.Setup(repository => repository.ExistActiveUserWithEmail(email)).ReturnsAsync(true);
+    }
+
+    public IUserReadOnlyRepository Build() => repository.Object;
+}
+
