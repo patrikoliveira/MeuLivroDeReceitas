ff8b8410e5bde2bf2e1f1cc4da0af28a598f67b3
Patrik Oliveira <patrikro.oliveira@gmail.com>
2024-05-06 19:02:37 -0300

    fix: adjustments to issues indicated by sonar

---
+++ src/Backend/MyRecipeBook.Domain/Extensions/StringExtension.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MyRecipeBook.Domain.Extensions;
+
+public static class StringExtension
+{
+    public static bool NotEmpty([NotNullWhen(true)] this string? value) => string.IsNullOrWhiteSpace(value).IsFalse();
+}
